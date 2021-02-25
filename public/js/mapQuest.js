da8f9bdfb1f985eb7001e547240c95f2740873fa
tmkolinek <77120701+tmkolinek@users.noreply.github.com>
2021-02-25 10:30:54 -0800

    finished

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,15 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = 'QADSjSIH8KqDZxudybBlfW8Zl9DWdTZ1'
+
+	var map = L.mapquest.map('map', {
+  center: [32.917, -117.230],
+  layers: L.mapquest.tileLayer('map'),
+  zoom: 12,
+  zoomControl: false
+
+  });
+
+
+	L.marker([32.917, -177.230]).addTo(map);
+
 }
